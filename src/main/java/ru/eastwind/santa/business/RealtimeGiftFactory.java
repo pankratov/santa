1add0cde6304e0b996b4c97b06e7706027261bba
v.pankratov <v.pankratov@eastwind.ru>
2015-12-29 19:40:28 +0500

    Фабрика

--- src/main/java/ru/eastwind/santa/business/RealtimeGiftFactory.java
+++ src/main/java/ru/eastwind/santa/business/RealtimeGiftFactory.java
@@ -1,5 +1,13 @@
 package ru.eastwind.santa.business;
 
+import java.util.HashMap;
+import java.util.WeakHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,14 +19,44 @@ public class RealtimeGiftFactory implements GiftFactory {
 	/**
 	 * количество эльфов
 	 */
-	private static int THREAD_NUMBER = 5;
+	private static int ELF_NUMBER = 5;
+
+	private static int ELF_LAZINESS = 2;
+
+	private ExecutorService executor;
+
+	private HashMap<String, Future<?>> processes = new HashMap<>();
 
 	@Autowired
 	private GiftStore giftStore;
 
+	public RealtimeGiftFactory() {
+		executor = Executors.newFixedThreadPool(ELF_NUMBER);
+	}
+
+	private void elfWork(String childName, String giftName) {
+		Logger.getGlobal().info("Hard working...");
+		Gift gift = createGift(giftName);
+		Logger.getGlobal().info("Stick label...");
+		gift.setLabel(childName);
+		Logger.getGlobal().info("Done!");
+		putAtStore(gift);
+	}
+
+	private Gift createGift(String giftName) {
+		try {
+			TimeUnit.SECONDS.sleep(ELF_LAZINESS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return new Gift(giftName);
+	}
+
 	@Override
 	public void createGiftForChild(String childName, String giftName) {
-		// TODO Auto-generated method stub
+		// lambda
+		Future<?> future = executor.submit(() -> elfWork(childName, giftName));
+		processes.put(childName, future);
 	}
 
 	private void putAtStore(Gift gift) {
@@ -27,7 +65,12 @@ public class RealtimeGiftFactory implements GiftFactory {
 
 	@Override
 	public void cancelForChild(String childName) {
-		// отменнить текущий процесс или вызывать unlabel
+		if (processes.containsKey(childName)) {
+			Future<?> future = processes.get(childName);
+			if (!future.isDone())
+				future.cancel(true);
+		}
+		giftStore.unlabelForChild(childName);
 	}
 
 }
